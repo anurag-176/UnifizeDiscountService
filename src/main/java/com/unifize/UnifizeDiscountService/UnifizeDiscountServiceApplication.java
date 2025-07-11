dc660a395320d74125a562e9939144b6b9cc955b
Anurag Bhalekar <anuragbhalekar17@gmail.com>
2025-07-11 17:25:19 +0530

    Spring Initializr commit

---
+++ src/main/java/com/unifize/UnifizeDiscountService/UnifizeDiscountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.unifize.UnifizeDiscountService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifizeDiscountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifizeDiscountServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unifize/UnifizeDiscountService/UnifizeDiscountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifize.UnifizeDiscountService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifizeDiscountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
